8778be1a51938b2667a6c9517e7d815bf4447c84
RuslanSakaev <hardist@mail.ru>
2022-11-06 18:53:30 +0300

    Отредактировал и закомментировал задачу в коде

--- FT/Program.cs
+++ FT/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Задача: 
+﻿// Задача: 
 // Написать программу, которая из имеющегося массива строк,
 // формирует массив из строк, 
 // длина которого меньше либо равна 3 символа.
