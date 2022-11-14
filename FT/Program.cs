450b052ce7ae32523a34649a2cf1305b507b0b77
RuslanSakaev <hardist@mail.ru>
2022-11-15 00:34:52 +0300

    Добавил решение задачи

--- FT/Program.cs
+++ FT/Program.cs
@@ -10,43 +10,31 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"] 
 // ["Russia", "Denmark", "Kazan"] -> []
 
-// Console.WriteLine("Введите n");
-// int n = int.Parse(Console.ReadLine());
-// int[] a = new int[n];
-// for (int i = 0; i < n; i++)
-// {
-//     Console.WriteLine("Введите {0}-й элемент", i + 1);
-//     a[i] = int.Parse(Console.ReadLine());
-// }
-// int k = 0;
-// for (int i = 0; i < a.Length; i++)
-//     if (a[i] % 2 == 0)
-//         k++;
-// Console.WriteLine(k);
-// Console.ReadKey();
 
+// string[] array = { "hello", "2", "world", ":-)" };
+// string[] array = { "1234", "1567", "-2", "computer science" };
+string[] array = { "Russia", "Denmark", "Kazan" };
 
-string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+string[] GetShortWordsArray(string[] array)
 {
+    int length = array.Length;
+    string[] result = new string[length];
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+
+    for (int i = 0; i < length; i++)
     {
-        if (array1[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-            array2[count] = array1[i];
+            result[count] = array[i];
             count++;
         }
     }
+    Array.Resize(ref result, count);
+
+    return result;
 }
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
+string[] arr = array;
+string[] shortArray = GetShortWordsArray(arr);
+Console.WriteLine("[{0}] -> [{1}]", string.Join(", ", arr), string.Join(", ", shortArray));
+
+
